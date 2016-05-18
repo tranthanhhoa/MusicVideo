e32baba6aa45894f3d61301b2b3a601f8aa21452
Tran Thanh Hoa <phamhoa.coder@gmail.com>
2016-05-19 00:11:38 +0700

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by Tran Thanh Hoa on 5/19/16.
+//  Copyright © 2016 Tran Thanh Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
